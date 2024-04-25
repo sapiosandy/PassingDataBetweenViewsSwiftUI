c5f747150b7845ba43d5d59197d470afd867282d
Sandy Gomez <sgomez1029@gmail.com>
2024-04-25 13:52:27 -0700

    Initial Commit

---
+++ PassingDataBetweenViewsSwiftUI/PassingDataBetweenViewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataBetweenViewsSwiftUIApp.swift
+//  PassingDataBetweenViewsSwiftUI
+//
+//  Created by Sandra Gomez on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PassingDataBetweenViewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
